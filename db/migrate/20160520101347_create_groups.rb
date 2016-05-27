34a30be464fb91b4f594647f2b102ab7a2d485e7
Tifany87 <inefual@yahoo.com.tw>
2016-05-27 23:28:09 +0800

    the first files about ROR

---
+++ db/migrate/20160520101347_create_groups.rb
@@ -0,0 +1,11 @@
+class CreateGroups < ActiveRecord::Migration
+  def change
+    create_table :groups do |t|
+      t.string :title
+      t.string :name
+      t.text :content
+
+      t.timestamps null: false
+    end
+  end
+end
